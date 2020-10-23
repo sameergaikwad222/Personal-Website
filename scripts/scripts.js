38c39a077818cf8479165f9e83cf8cb2545de7d7
SAMEER GAIKWAD <sameergaikwad222@hotmail.com>
2020-10-23 23:53:08 +0530

    index page updated & improved

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,6 +7,63 @@ var open = document.getElementById("navToggle");
 open.addEventListener("click", nav);
 open.addEventListener("click", menu);
 
+
+// HTML COLOR CHANGES ON HOVER
+var htmlicon = document.getElementById("htmlIcon");
+var cssicon = document.getElementById("cssIcon");
+var jsicon = document.getElementById("jsIcon");
+var booticon = document.getElementById("bootIcon");
+var javaicon = document.getElementById("javaIcon");
+
+htmlicon.addEventListener("mouseover",getColor);
+htmlicon.addEventListener("mouseout",retColor);
+cssicon.addEventListener("mouseover",getColor);
+cssicon.addEventListener("mouseout",retColor);
+jsicon.addEventListener("mouseover",getColor);
+jsicon.addEventListener("mouseout",retColor);
+booticon.addEventListener("mouseover",getColor);
+booticon.addEventListener("mouseout",retColor);
+javaicon.addEventListener("mouseover",getColor);
+javaicon.addEventListener("mouseout",retColor);
+
+function retColor() {
+  // var element = document.getElementById("htmlIcon")
+  // csselement = document.getElementById("htmlIcon")
+  this.classList.remove("colored");
+}
+
+function getColor() {
+  // var element = document.getElementById("htmlIcon")
+  this.classList.add("colored");
+}
+
+
+
+
+
+// // HTML COLOR CHANGES ON HOVER
+// var htmlicon = document.getElementById("htmlIcon");
+// htmlicon.addEventListener("mouseover",getColor('htmlIcon'));
+// htmlicon.addEventListener("mouseout",retColor('htmlIcon'));
+
+
+
+// function getColor(selID) {
+//   var element = document.getElementById(selID)
+//   element.classList.add("colored");
+// }
+
+
+// function retColor() {
+//   var element = document.getElementById("htmlIcon")
+//   this.classList.remove("colored");
+// }
+
+
+
+
+
+
 function nav() {
   var element = document.getElementById("logo");
   element.classList.toggle("menu");
